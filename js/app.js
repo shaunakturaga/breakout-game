901d8e72ad03a16ea39abec80e0f6829883cb3b6
shaunakturaga <shaunak.turaga@capitalone.com>
2017-08-22 14:18:23 -0400

    Display brick field

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 window.onload = function() {
+  // Grab the canvas element & reference context
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
+  // Position variables
   var x = canvas.width/2;
   var y = canvas.height-30;
   var dx = 2;
@@ -31,6 +33,41 @@ window.onload = function() {
     ctx.closePath();
   }
 
+  // Render the brick field
+  var brickRows =3;
+  var brickColumns = 9;
+  var brickWidth = 65;
+  var brickHeight = 20;
+  var brickPadding = 10;
+  var brickOffsetTop = 30;
+  var brickOffsetLeft = 50;
+
+  var bricks = [];
+  for(var col = 0; col<brickColumns;col++){
+    bricks[col] = [];
+    for(var row = 0; row<brickRows;row++){
+      bricks[col][row] = {x:0, y:0, status: 1};
+    }
+  }
+
+  function drawBricksField() {
+    for(var col =0; col<brickColumns;col++){
+      for(var row=0; row<brickRows;row++){
+        if(bricks[col][row].status == 1){
+          var brickX = (col*(brickWidth+brickPadding))+brickOffsetLeft;
+          var brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
+          bricks[col][row].x = 0;
+          bricks[col][row].y = 0;
+          ctx.beginPath();
+          ctx.rect(brickX,brickY,brickWidth,brickHeight);
+          ctx.fillStyle = "#E45641";
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+
   // User control
   var rightPressed = false;
   var leftPressed = false;
@@ -57,11 +94,25 @@ window.onload = function() {
         }
     }
 
+    // Collision Detection
+    function collisionDetection(){
+      for(var col=0; col<brickColumns; col++){
+        for(var row=0;row<brickRows;row++){
+          var b = bricks[col][row];
+          if(x>b.x && x< b.x+brickWidth && y>b.y && y <b.y+brickHeight){
+            dy = -dy;
+          }
+        }
+      }
+    }
+
   // Render the gameboard & elements
   function draw() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
     drawPaddle();
     createBall();
+    drawBricksField();
+    collisionDetection();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
       dx = -dx;
@@ -74,7 +125,7 @@ window.onload = function() {
         dy = -dy;
       }
       else {
-        alert("GAME OVER");
+        // alert("GAME OVER");
         document.location.reload();
       }
     }
