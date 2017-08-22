f508416f43abd92c8205e988f6dd6b4120363829
shaunakturaga <shaunak.turaga@capitalone.com>
2017-08-22 14:54:23 -0400

    Ball can destroy bricks

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ window.onload = function() {
   }
 
   // Render the brick field
-  var brickRows =3;
+  var brickRows = 3;
   var brickColumns = 9;
   var brickWidth = 65;
   var brickHeight = 20;
@@ -43,29 +43,29 @@ window.onload = function() {
   var brickOffsetLeft = 50;
 
   var bricks = [];
-  for(var col = 0; col<brickColumns;col++){
+  for(col=0; col<brickColumns; col++) {
     bricks[col] = [];
-    for(var row = 0; row<brickRows;row++){
-      bricks[col][row] = {x:0, y:0, status: 1};
+    for(row=0; row<brickRows; row++) {
+         bricks[col][row] = { x: 0, y: 0, status: 1 };
     }
   }
 
   function drawBricksField() {
-    for(var col =0; col<brickColumns;col++){
-      for(var row=0; row<brickRows;row++){
-        if(bricks[col][row].status == 1){
-          var brickX = (col*(brickWidth+brickPadding))+brickOffsetLeft;
-          var brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
-          bricks[col][row].x = 0;
-          bricks[col][row].y = 0;
-          ctx.beginPath();
-          ctx.rect(brickX,brickY,brickWidth,brickHeight);
-          ctx.fillStyle = "#E45641";
-          ctx.fill();
-          ctx.closePath();
-        }
+      for(col=0; col<brickColumns; col++) {
+          for(row=0; row<brickRows; row++) {
+              if(bricks[col][row].status == 1) {
+                  var brickX = (col*(brickWidth+brickPadding))+brickOffsetLeft;
+                  var brickY = (row*(brickHeight+brickPadding))+brickOffsetTop;
+                  bricks[col][row].x = brickX;
+                  bricks[col][row].y = brickY;
+                  ctx.beginPath();
+                  ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                  ctx.fillStyle = "#E45641";
+                  ctx.fill();
+                  ctx.closePath();
+              }
+          }
       }
-    }
   }
 
   // User control
@@ -96,11 +96,14 @@ window.onload = function() {
 
     // Collision Detection
     function collisionDetection(){
-      for(var col=0; col<brickColumns; col++){
-        for(var row=0;row<brickRows;row++){
+      for(col=0; col<brickColumns; col++){
+        for(row=0;row<brickRows;row++){
           var b = bricks[col][row];
-          if(x>b.x && x< b.x+brickWidth && y>b.y && y <b.y+brickHeight){
-            dy = -dy;
+          if(b.status == 1){
+            if(x> b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
+              dy = -dy;
+              b.status = 0;
+            }
           }
         }
       }
@@ -109,9 +112,9 @@ window.onload = function() {
   // Render the gameboard & elements
   function draw() {
     ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawBricksField();
     drawPaddle();
     createBall();
-    drawBricksField();
     collisionDetection();
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
@@ -125,7 +128,7 @@ window.onload = function() {
         dy = -dy;
       }
       else {
-        // alert("GAME OVER");
+        alert("GAME OVER");
         document.location.reload();
       }
     }
